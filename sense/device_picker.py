7475ff4ef41f596b55580eb1cba5869a48105ea8
Afonso Raposo <afonsocraposo@gmail.com>
2022-02-24 19:17:37 +0000

    Fixed device picker linux

--- sense/device_picker.py
+++ sense/device_picker.py
@@ -24,7 +24,7 @@ class DevicePicker:
 
     def __get_device_options(self):
         if sys.platform == "linux":
-            options = self.get_linux_bth_devices()
+            options = self.__get_linux_bth_devices()
             return list(map(lambda option: option["addr"], options)), list(
                 map(
                     lambda option: "{} - {}".format(option["name"], option["addr"]),
